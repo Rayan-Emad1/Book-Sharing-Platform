9cb350065f069d12894eb0d22fa03e6f74532115
Rayan <rayanemad508@gmail.com>
2023-08-24 01:52:51 +0300

    BE - created routes

--- Server-nodejs/routes/users.routes.js
+++ Server-nodejs/routes/users.routes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const usersController = require("../controllers/users.controllers");
 const authMiddleware = require("../middlewares/auth.middleware");
 
-router.post("/add_book", authMiddleware , usersController.postFavoriteBook);
-router.post("/add_book", authMiddleware , usersController.postFavoriteBook);
-router.post("/add_book", authMiddleware , usersController.postFavoriteBook);
+router.post("/get_user_info", authMiddleware , usersController.userInfo);
+router.post("/add_book", authMiddleware , usersController.postBook);
 
+router.post("/search_user", authMiddleware , usersController.searchUser);
+router.post("/follow_user", authMiddleware , usersController.togglefollowUser);
+
+router.get("/following_books", authMiddleware , usersController.getBooks);
+router.post("/like_book", authMiddleware , usersController.toggleLikeBook);
   
 module.exports = router;
