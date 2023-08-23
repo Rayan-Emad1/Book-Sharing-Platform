432e160830e186ab9c4bec954f3dc23cba82c904
Rayan <rayanemad508@gmail.com>
2023-08-23 22:30:38 +0300

    BE - added userInfo

--- Server-nodejs/controllers/auth.controllers.js
+++ Server-nodejs/controllers/auth.controllers.js
@@ -9,8 +9,8 @@ const login = async (req, res)=>{
     const isValid = await bcrypt.compare(password, user.password);
     if(!isValid) return res.status(404).send({message: "email/password incorrect"});
 
-    const {password: hashedPassword, firstName, lastName, email, _id, ...userInfo} = user.toJSON();
-    const token = jwt.sign({firstName, lastName, email, _id}, process.env.JWT_SECRET)
+    const {password: hashedPassword, username, email, _id, ...userInfo} = user.toJSON();
+    const token = jwt.sign({username, email, _id}, process.env.JWT_SECRET)
 
     res.send({
         token,
@@ -21,7 +21,7 @@ const login = async (req, res)=>{
 
 const register = async (req, res) => {
     try {
-        const { password } = req.body;
+        const {username,email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({
             ...req.body,
@@ -30,7 +30,14 @@ const register = async (req, res) => {
 
         await user.save();
 
-        res.status(201).json({ message: "User registered successfully" });
+        const tokenPayload = {
+            _id: user._id,
+            username,
+            email
+          };
+          const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
+
+        res.status(201).json({ message: "User registered successfully", "token" : token});
     } catch (error) {
         res.status(500).json({ 
             error: error.message,});
--- Server-nodejs/controllers/users.controllers.js
+++ Server-nodejs/controllers/users.controllers.js
@@ -1,33 +1,59 @@
 const User = require("../models/users.model")
 
 
-const postFavoriteBook = async (req, res) => {
-    try {
-      const { title, author, pictureUrl, review } = req.body;
-      const user = await User.findById(req.user._id);
-  
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-  
-      user.favoriteBooks.push({
-        title,
-        author,
-        genre,
-        pictureUrl,
-        review,
-      });
-  
-      await user.save();
-  
-      res.status(201).json({ message: "Book added to favorites" });
-    } catch (error) {
-      res.status(500).json({ error: "An error occurred" });
+const postBook = async (req, res) => {
+  try {
+    const { title, author, pictureUrl, review, genre } = req.body;
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-  };
 
+    const newBook = {
+      title,
+      author,
+      genre,
+      pictureUrl,
+      review,
+      user: user._id,
+    };
 
+    user.ownBooks.push(newBook);
+    await user.save();
+    res.status(200).json({ message: "Book created", "newBook": newBook });
 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+  
+const userInfo = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const userData = await User.findById(userId)
+      .populate('favoriteBooks')
+      .populate('following');
+    console.log(userId);
+    const ownLikesCount = userData.ownBooks.reduce((total, book) => total + book.likes.length, 0);
+    const ownBooksCount = userData.ownBooks.length;
+    const followingCount = userData.following.length;
+    const ownBooks = userData.ownBooks;
+    const favoriteBooks = userData.favoriteBooks;
 
+    const userInfo = {
+      username: userData.username,
+      ownLikesCount,
+      ownBooksCount,
+      followingCount,
+      ownBooks,
+      favoriteBooks,
+    };
 
-module.exports = {postFavoriteBook}
+    res.json(userInfo);
+  } catch (error) {
+    console.error(error);
+    res.json(userInfo);
+  }
+}
+module.exports = {userInfo, postBook}
