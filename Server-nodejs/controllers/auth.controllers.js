36b3b774df23d8ee5f64a688d0233b8695984534
Rayan <rayanemad508@gmail.com>
2023-08-23 20:21:22 +0300

    BE - edited auth controller

--- Server-nodejs/controllers/auth.controllers.js
+++ Server-nodejs/controllers/auth.controllers.js
@@ -19,20 +19,23 @@ const login = async (req, res)=>{
 
 }
 
-const register = async(req, res)=>{
-    const {password} = req.body
+const register = async (req, res) => {
+    try {
+        const { password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            ...req.body,
+            password: hashedPassword
+        });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-        ...req.body,
-        password: hashedPassword
-    });
+        await user.save();
 
-    user.save()
-
-    res.send(user)
-    
-}
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ 
+            error: error.message,});
+    }
+};
 
 const verify = (_, res)=>{
     res.send("Verfied")
--- Server-nodejs/index.js
+++ Server-nodejs/index.js
@@ -3,10 +3,23 @@ const app = express();
 const mongooseConnect = require("./configs/mongoDB.connect");
 require("dotenv").config();
 
+app.use(express.json());
+
+
+
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
 
-app.use(express.json());
+const userRoutes = require("./routes/users.routes");
+app.use("/users", userRoutes);
+
+
+
+
+console.log("rr")
+
+
+
 
 app.listen(8000, (err) => {
   if (err) {
